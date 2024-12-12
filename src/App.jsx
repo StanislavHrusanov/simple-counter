4a43b7b663398a7c1b0d71641ac749d1dccb2973
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-12-12 14:44:03 +0200

    Adding functionality to increment, decrement and reset the counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
 
+  function incrementHandler() {
+    setCounter((state) => state + 1);
+  }
+
+  function decrementHandler() {
+    setCounter((state) => state - 1);
+  }
+
+  function resetHandler() {
+    setCounter(0);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <main className={styles.main}>
+      <div className={styles.counter}>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Counter:<span>{counter}</span>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className={styles.buttons}>
+        <button onClick={decrementHandler}>-</button>
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={resetHandler}>Reset</button>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
